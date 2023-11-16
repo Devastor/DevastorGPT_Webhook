0f1ca94bb8f296740208042959fa39e87ed71845
Devastor <ahmetovv@mail.ru>
2023-11-17 02:56:34 +0300

    Add files via upload

---
+++ DevastorChatGPT_Webhook.py
@@ -0,0 +1,79 @@
+
+import openai
+import os
+import openai
+import telebot
+import cherrypy
+
+# Наш вебхук-сервер
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # Эта функция обеспечивает проверку входящего сообщения
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+token = ''
+openai.api_key = ''
+         
+users = {1234567890, 1234567891}
+
+accepted_users = lambda message: message.from_user.id not in users
+
+max_symbols = lambda message: int(len(message.text)) > 2000
+
+WEBHOOK_HOST = '192.168.0.1' # вставить адрес сервера для вебхуков
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (token)
+
+bot = telebot.TeleBot(token)
+
+# Хэндлер на все текстовые сообщения
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    #bot.reply_to(message, message.text)
+
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=4000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    bot.send_message(message.chat.id, response['choices'][0]['text'])
+    #message.answer(response['choices'][0]['text'])
+
+# Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+bot.remove_webhook()
+
+# Ставим заново вебхук
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+# Указываем настройки сервера CherryPy
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+ # Собственно, запуск!
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
